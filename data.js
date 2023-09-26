7e9d1c75ce40a1e91de65c5d59e913752fbd346e
Vishwanath <125250998+chiruvaradani@users.noreply.github.com>
2023-09-26 16:36:16 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,74 @@
+export const data=[
+    {
+        image:'https://res.cloudinary.com/chiruvaradani/image/upload/v1691655561/dtmyxez5gtwdnky4pe41.png',
+        name:'Shoes',
+        location:'Gulbarga',
+        caption:'Build a Social Media App in React Native using Nodejs & MongoDB ( Day - 2 ) | React Native Projects',
+        time:'2 mins ago',
+        NoOfLikes:10,
+        LikedBy:'Anu, jack +8 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/Salomonimage._CB589992930_.jpg',
+        name:'Shoes2',
+        location:'Gadag',
+        caption:'wondering about future',
+        time:'15 mins ago',
+        NoOfLikes:20,
+        LikedBy:'Anu, jack +18 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/NB._CB589992930_.jpg',
+        name:'Shoes',
+        location:'Gulbarga',
+        caption:'Build a Social Media App in React Native using Nodejs & MongoDB ( Day - 2 ) | React Native Projects',
+        time:'2 mins ago',
+        NoOfLikes:10,
+        LikedBy:'Anu, jack +8 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/Salomonimage._CB589992930_.jpg',
+        name:'Shoes2',
+        location:'Gadag',
+        caption:'wondering about future',
+        time:'15 mins ago',
+        NoOfLikes:20,
+        LikedBy:'Anu, jack +18 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/NB._CB589992930_.jpg',
+        name:'Shoes',
+        location:'Gulbarga',
+        caption:'Build a Social Media App in React Native using Nodejs & MongoDB ( Day - 2 ) | React Native Projects',
+        time:'2 mins ago',
+        NoOfLikes:10,
+        LikedBy:'Anu, jack +8 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/Salomonimage._CB589992930_.jpg',
+        name:'Shoes2',
+        location:'Gadag',
+        caption:'wondering about future',
+        time:'15 mins ago',
+        NoOfLikes:20,
+        LikedBy:'Anu, jack +18 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/NB._CB589992930_.jpg',
+        name:'Shoes',
+        location:'Gulbarga',
+        caption:'Build a Social Media App in React Native using Nodejs & MongoDB ( Day - 2 ) | React Native Projects',
+        time:'2 mins ago',
+        NoOfLikes:10,
+        LikedBy:'Anu, jack +8 more'
+    },
+    {
+        image:'https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Newlaunch/Salomonimage._CB589992930_.jpg',
+        name:'Shoes2',
+        location:'Gadag',
+        caption:'wondering about future',
+        time:'15 mins ago',
+        NoOfLikes:20,
+        LikedBy:'Anu, jack +18 more'
+    },
+]
